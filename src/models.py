58baef8d7f52c55e80b91d1cfb3652984cc84e3a
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2026-03-19 19:32:47 -0300

    Refactor: adicao de pesos ao grafo

--- src/models.py
+++ src/models.py
@@ -3,17 +3,21 @@ class Graph:
         self.adj_list = {}
         self.directed = directed
 
-    def add_edge(self, u, v):
-        for node in [u, v]:
-            if node not in self.adj_list:
-                self.adj_list[node] = set()
+    def add_edge(self, u, v, weight=1):
+        if u not in self.adj_list:
+            self.adj_list[u] = {}
+        if v not in self.adj_list:
+            self.adj_list[v] = {}
 
-        self.adj_list[u].add(v)
+        self.adj_list[u][v] = weight
         if not self.directed:
-            self.adj_list[v].add(u)
+            self.adj_list[v][u] = weight
 
     def get_nodes(self):
         return list(self.adj_list.keys())
 
     def get_neighbors(self, node):
-        return self.adj_list.get(node, set())
+        return list(self.adj_list.get(node, {}).keys())
+    
+    def get_weight(self, u, v):
+        return self.adj_list.get(u, {}).get(v)
