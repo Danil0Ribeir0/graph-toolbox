3bd8a13acd26b355dcaabb24e768827cf9151ceb
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2026-03-23 17:58:37 -0300

    remocao do GraphTransversal

--- src/algorithms.py
+++ src/algorithms.py
@@ -2,27 +2,6 @@ import heapq
 from typing import Dict, Optional, Tuple, Hashable, List, Set
 from src.models import Graph
 
-
-class GraphTraversal:
-    @staticmethod
-    def is_connected(graph):
-        nodes = graph.get_nodes()
-        if not nodes:
-            return True
-
-        visited = set()
-        start_node = nodes[0]
-
-        def dfs(v):
-            visited.add(v)
-            for neighbor in graph.get_neighbors(v):
-                if neighbor not in visited:
-                    dfs(neighbor)
-
-        dfs(start_node)
-        return len(visited) == len(nodes)
-
-
 class PathFinder:
     @staticmethod
     def dijkstra(graph: Graph, start_node: Hashable) -> Dict[Hashable, float]:
